6e1480cf1cbe021820449e8fdbeeb5b57d10be65
Máté <szabo.mate@inf.unideb.hu>
2025-09-23 15:20:40 +0200

    init

---
+++ src/main/java/hu/unideb/inf/autokolcsonzo/AutokolcsonzoApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.autokolcsonzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutokolcsonzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutokolcsonzoApplication.class, args);
+	}
+
+}
